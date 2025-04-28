a994fa10b01329499c668466eaf8c8323b6d0cb7
YouSiki <you.siki@outlook.com>
2025-04-28 13:23:13 +0800

    Add the first python solution

---
+++ README.md
@@ -0,0 +1,3 @@
+# Leetcode solutions
+
+This repository contains my solutions to Leetcode problems.
---
+++ python/solutions/p2302_count_subarrays_with_score_less_than_k.py
@@ -0,0 +1,98 @@
+#
+# @lc app=leetcode.cn id=2302 lang=python3
+#
+# [2302] Count Subarrays With Score Less Than K
+#
+# https://leetcode.cn/problems/count-subarrays-with-score-less-than-k/description/
+#
+# algorithms
+# Hard (58.62%)
+# Likes:    75
+# Dislikes: 0
+# Total Accepted:    21.6K
+# Total Submissions: 35.5K
+# Testcase Example:  '[2,1,4,3,5]\n10'
+#
+# The score of an array is defined as the product of its sum and its
+# length.
+#
+#
+# For example, the score of [1, 2, 3, 4, 5] is (1 + 2 + 3 + 4 + 5) * 5 = 75.
+#
+#
+# Given a positive integer array nums and an integer k, return the number of
+# non-empty subarrays of nums whose score is strictly less than k.
+#
+# A subarray is a contiguous sequence of elements within an array.
+#
+#
+# Example 1:
+#
+#
+# Input: nums = [2,1,4,3,5], k = 10
+# Output: 6
+# Explanation:
+# The 6 subarrays having scores less than 10 are:
+# - [2] with score 2 * 1 = 2.
+# - [1] with score 1 * 1 = 1.
+# - [4] with score 4 * 1 = 4.
+# - [3] with score 3 * 1 = 3.
+# - [5] with score 5 * 1 = 5.
+# - [2,1] with score (2 + 1) * 2 = 6.
+# Note that subarrays such as [1,4] and [4,3,5] are not considered because
+# their scores are 10 and 36 respectively, while we need scores strictly less
+# than 10.
+#
+# Example 2:
+#
+#
+# Input: nums = [1,1,1], k = 5
+# Output: 5
+# Explanation:
+# Every subarray except [1,1,1] has a score less than 5.
+# [1,1,1] has a score (1 + 1 + 1) * 3 = 9, which is greater than 5.
+# Thus, there are 5 subarrays having scores less than 5.
+#
+#
+#
+# Constraints:
+#
+#
+# 1 <= nums.length <= 10^5
+# 1 <= nums[i] <= 10^5
+# 1 <= k <= 10^15
+#
+#
+#
+
+from typing import List
+
+
+# @lc code=start
+class Solution:
+  def countSubarrays(self, nums: List[int], k: int) -> int:
+    answer = 0
+    left, queue_sum, queue_score = 0, 0, 0
+    for right, nums_right in enumerate(nums):
+      # Add nums[right] to the queue
+      queue_sum += nums_right
+      queue_score = queue_sum * (right - left + 1)
+      # Pop elements from queue until score < k
+      while queue_score >= k:
+        queue_sum -= nums[left]
+        left += 1
+        queue_score = queue_sum * (right - left + 1)
+      # Add the number of valid subarrays
+      answer += right - left + 1
+    return answer
+
+
+# @lc code=end
+
+
+def test_example_1():
+  assert Solution().countSubarrays([2, 1, 4, 3, 5], 10) == 6
+
+
+def test_example_2():
+  assert Solution().countSubarrays([1, 1, 1], 5) == 5
