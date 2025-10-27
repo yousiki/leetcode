09c17570231310217c15ac50ac82034152b5bf75
yousiki <you.siki@outlook.com>
2025-10-27 13:38:30 +0800

    Add solution for LeetCode problem: Number of Laser Beams in a Bank and update Cargo.toml

---
+++ rust/solutions/number_of_laser_beams_in_a_bank.rs
@@ -0,0 +1,124 @@
+/*
+ * @lc app=leetcode id=2125 lang=rust
+ *
+ * [2125] Number of Laser Beams in a Bank
+ *
+ * https://leetcode.com/problems/number-of-laser-beams-in-a-bank/description/
+ *
+ * algorithms
+ * Medium (85.40%)
+ * Likes:    2011
+ * Dislikes: 203
+ * Total Accepted:    253.6K
+ * Total Submissions: 295K
+ * Testcase Example:  '["011001","000000","010100","001000"]'
+ *
+ * Anti-theft security devices are activated inside a bank. You are given a
+ * 0-indexed binary string array bank representing the floor plan of the bank,
+ * which is an m x n 2D matrix. bank[i] represents the i^th row, consisting of
+ * '0's and '1's. '0' means the cell is empty, while'1' means the cell has a
+ * security device.
+ *
+ * There is one laser beam between any two security devices if both conditions
+ * are met:
+ *
+ *
+ * The two devices are located on two different rows: r1 and r2, where r1 <
+ * r2.
+ * For each row i where r1 < i < r2, there are no security devices in the i^th
+ * row.
+ *
+ *
+ * Laser beams are independent, i.e., one beam does not interfere nor join with
+ * another.
+ *
+ * Return the total number of laser beams in the bank.
+ *
+ *
+ * Example 1:
+ *
+ *
+ * Input: bank = ["011001","000000","010100","001000"]
+ * Output: 8
+ * Explanation: Between each of the following device pairs, there is one beam.
+ * In total, there are 8 beams:
+ * ⁠* bank[0][1] -- bank[2][1]
+ * ⁠* bank[0][1] -- bank[2][3]
+ * ⁠* bank[0][2] -- bank[2][1]
+ * ⁠* bank[0][2] -- bank[2][3]
+ * ⁠* bank[0][5] -- bank[2][1]
+ * ⁠* bank[0][5] -- bank[2][3]
+ * ⁠* bank[2][1] -- bank[3][2]
+ * ⁠* bank[2][3] -- bank[3][2]
+ * Note that there is no beam between any device on the 0^th row with any on
+ * the 3^rd row.
+ * This is because the 2^nd row contains security devices, which breaks the
+ * second condition.
+ *
+ *
+ * Example 2:
+ *
+ *
+ * Input: bank = ["000","111","000"]
+ * Output: 0
+ * Explanation: There does not exist two devices located on two different
+ * rows.
+ *
+ *
+ *
+ * Constraints:
+ *
+ *
+ * m == bank.length
+ * n == bank[i].length
+ * 1 <= m, n <= 500
+ * bank[i][j] is either '0' or '1'.
+ *
+ *
+ */
+
+struct Solution;
+
+// @lc code=start
+impl Solution {
+    pub fn number_of_beams(bank: Vec<String>) -> i32 {
+        bank.iter()
+            .map(|row| row.chars().filter(|&c| c == '1').count() as i32)
+            .filter(|&cnt| cnt > 0)
+            .collect::<Vec<_>>()
+            .windows(2)
+            .map(|x| x[0] * x[1])
+            .sum()
+    }
+}
+// @lc code=end
+
+#[cfg(test)]
+mod test_number_of_laser_beams_in_a_bank {
+    use super::*;
+
+    #[test]
+    fn test1() {
+        assert_eq!(
+            Solution::number_of_beams(vec![
+                "011001".to_string(),
+                "000000".to_string(),
+                "010100".to_string(),
+                "001000".to_string()
+            ]),
+            8
+        );
+    }
+
+    #[test]
+    fn test2() {
+        assert_eq!(
+            Solution::number_of_beams(vec![
+                "000".to_string(),
+                "111".to_string(),
+                "000".to_string()
+            ]),
+            0
+        );
+    }
+}
